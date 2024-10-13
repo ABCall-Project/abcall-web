4521971f82adfab0e9a51a9594c6a741887c1056
fabioCastagnola <f.castagnola@uniandes.edu.co>
2024-10-12 20:51:06 -0500

    Formulario base

--- src/app/pages/authentication/side-register/side-register.component.ts
+++ src/app/pages/authentication/side-register/side-register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CoreService } from 'src/app/services/core.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -12,14 +12,19 @@ import { MaterialModule } from 'src/app/material.module';
   standalone: true,
   imports: [RouterModule, MaterialModule, NgIf, FormsModule, ReactiveFormsModule],
   templateUrl: './side-register.component.html',
+  styleUrls: ['./side-register.component.scss']
 })
 export class AppSideRegisterComponent {
   options = this.settings.getOptions();
 
-  constructor(private settings: CoreService, private router: Router) {}
+  constructor(private settings: CoreService, private router: Router) { }
 
   form = new FormGroup({
-    uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    nombres: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    telefono: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    canal: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    nit: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
     email: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
   });
