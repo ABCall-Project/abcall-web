97a47082b9e8d6c0ed174a604729f47a0d50f0b9
fabioCastagnola <f.castagnola@uniandes.edu.co>
2024-10-31 02:37:05 -0500

    Add tests for services

--- src/app/services/customers/customers.service.spec.ts
+++ src/app/services/customers/customers.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { CustomersService } from './customers.service';
@@ -6,7 +7,10 @@ describe('CustomersService', () => {
   let service: CustomersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Ensure this is included
+      providers: [CustomersService],
+    });
     service = TestBed.inject(CustomersService);
   });
 
