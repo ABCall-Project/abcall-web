9286926b78a3f7bdde26cda2556afd67b3991637
fabioCastagnola <f.castagnola@uniandes.edu.co>
2024-11-01 20:17:19 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,10 @@ export const environment = {
     ...{
         production: true,
         environment: 'production',
-        ApiBase: 'http://172.212.38.79:3002'
+        ApiBase: 'http://172.212.38.79:3002',
+        ApiCustomers: 'http://localhost:5003',
+        ApiUsers: 'http://localhost:5004',
+        ApiIssues: 'http://localhost:5007'
     },
     ...paymentUrls,
     ...issuesUrls,
