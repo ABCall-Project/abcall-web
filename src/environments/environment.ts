7f5e438a7e0aaadf8072fdf5928b8895c821f9c3
zearkiatos <caprilespe@outlook.com>
2024-11-17 22:17:15 -0300

    Mapping default phrase key for testing and local environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,8 @@ export const environment = {
         ApiBase: 'http://localhost:5002',
         ApiCustomers: 'http://localhost:5003',
         ApiUsers: 'http://localhost:5004',
-        ApiIssues: 'http://localhost:5007'
+        ApiIssues: 'http://localhost:5007',
+        PHRASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZpcm9ubWVudCI6InRlc3QifQ.oH-jIKbyWL6jjH8YCk5YIEwnlnuB9f-5nXBYNFe3pXY'
     },
     ...paymentUrls,
     ...issuesUrls,
