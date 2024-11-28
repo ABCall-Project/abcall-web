3a5de39d4567991897aa87d0c50246414b2cdce8
zearkiatos <caprilespe@outlook.com>
2024-11-27 22:53:30 -0300

    Fixing version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ import { issuesUrls } from "./environment.issues"
 import { customersUrls } from "./environment.customers";
 import { usersUrls } from "./environment.users";
 import { config } from './env';
+const configPackage = require('../../package.json');
 
 export const environment = {
     ...{
@@ -10,6 +11,7 @@ export const environment = {
         environment: 'production',
         ApiBase: 'http://172.212.38.79:3002',
         PHRASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZpcm9ubWVudCI6InByb2R1Y3Rpb24iLCJuYW1lIjoiYWJjYWxsLWF1dGgtYXBpIn0.GmzwNvnOFDHdYnnsqj2nOIKn5199e2Kg7rNSJXWzrHQ',
+        VERSION: configPackage.version
     },
     ...paymentUrls,
     ...issuesUrls,
